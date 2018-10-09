3e92085a4525c7965e5b2b1402511a160fb70b2e
BraisMorgade <36987428+BraisMorgade@users.noreply.github.com>
2018-10-09 23:44:08 +0200

    1st version of class strucure

--- src/js/main.js
+++ src/js/main.js
@@ -40,3 +40,157 @@ window.onload = function () {
 
   game.state.start('boot');
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var Inkling= function (position, sprite, color, size, health, ink, player){
+  this._position=position;
+  this._sprite=sprite;
+  this._color=color;
+  this._size=size;
+  this._health=health;
+  this._ink=ink;
+  this._player=player;
+}
+
+Inkling.prototype.Movement=function(){};
+Inkling.prototype.Die=function(){};
+Inkling.prototype.Respawn=function(){};
+Inkling.prototype.Transform=function(){};
+Inkling.prototype.Jump=function(){};
+Inkling.prototype.Damage=function(){};
+
+
+var Kid= function (position, sprite, color, size, health, ink, player,aimpoint){
+  Inkling.apply(this,[position, sprite, color, size, health, ink, player]);
+  this._aimpoint= aimpoint;
+}
+
+Kid.prototype= Object.create(Inkling.prototype);
+Kid.prototype.constructor=Kid;
+
+Kid.prototype.Shoot=function(){};
+Kid.prototype.Aim=function(){};
+
+
+var Squid =function(position, sprite, color, size, health, ink, player){
+  Inkling.apply(this,[position, sprite, color, size, health, ink, player]);
+}
+Squid.prototype=Object.create(Inkling.prototype);
+Squid.prototype.constructor=Squid;
+
+Squid.prototype.Recharge=function(){};
+Squid.prototype.Swim=function(){};
+//modificar algunas funciones de Inkling
+
+
+
+
+
+
+
+
+var Platform = function(position, size, color, sprite){
+  this._position=position;
+  this._size=size;
+  this._color=color;
+  this._sprite=sprite;
+}
+
+Platform.prototype.ChangeColor=function(){};
+
+
+var MobilePlatform= function(position, size, color, sprite, direction, speed){
+  Platform.apply(this, [position, size, color, sprite]);
+  this._direction=direction;
+  this._speed=speed;
+}
+
+MobilePlatform.prototype=Object.create(Platform.prototype);
+MobilePlatform.prototype.constructor=MobilePlatform;
+
+MobilePlatform.prototype.Move=function(){};
+
+
+var Grilles= function(position, size, color, sprite){
+  Platform.apply(this, [position, size, color, sprite]);
+}
+
+Grilles.prototype=Object.create(Platform.prototype);
+Grilles.prototype.constructor=Grilles;
+Grilles.prototype.Cross= function(){};
+
+
+
+
+
+
+
+var Spawn= function(position, size, color, sprite){
+  this._position=position;
+  this._size=size;
+  this._color=color;
+  this._sprite=sprite;
+}
+
+Spawn.prototype.GiveInvulnerability=function(){};
+
+
+
+
+
+
+
+
+var Shot=function(position,color,sprite,direction){
+  this._position=position;
+  this._color=color;
+  this._sprite=sprite;
+  this._direction=direction;
+}
+
+Shot.prototype._bulletsize=x;
+Shot.prototype.Movement=function(){};
+Shot.prototype.Impact=function(){};
+
+
+
+
+

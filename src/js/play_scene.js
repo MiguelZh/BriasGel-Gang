76a7299986fc8de400283c883f638e4f2f5d8f61
BraisMorgade <36987428+BraisMorgade@users.noreply.github.com>
2018-11-02 19:02:14 +0100

    Epic StateMachine

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,4 +1,5 @@
-'use strict';
+'use strict'
+
 var Inkling= function (game, x, y, sprite, speed, jump){
   //Atributos
   Phaser.Sprite.call(this, game, x, y, sprite);
@@ -6,6 +7,8 @@ var Inkling= function (game, x, y, sprite, speed, jump){
   this._jump=jump;
   this._health=100;
   this._ammo=100;
+
+
 
 
   //Físicas
@@ -22,6 +25,7 @@ var Inkling= function (game, x, y, sprite, speed, jump){
   this.animations.add('idle',[0,1,2,3,4,5,6,7,8,9], 9, true);
   this.animations.add('jump', [10,11,12,13,14,15], 9, true);
   this.animations.add('run', [20,21,22,23,24,25,26,27], 9, true);
+  this.animations.add('transform', [30,31,32,33,34], 9, true);
   this.anchor.setTo(.5,.5);
   this.scale.setTo(this.scale.x * 2, this.scale.y *2);
   
@@ -36,28 +40,33 @@ Inkling.prototype.update=function(){
   if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) dir=-1;
   else if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) dir=1;
   this.Movement(dir);
+  if(this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+    console.log("aaa");
+    this.animations.play('transform', 9, false);
+  } 
+  else if(this.body.onFloor() && this.body.velocity.x===0) this.animations.play('idle');
+  else if(this.body.onFloor() && this.body.velocity.x !==0) this.animations.play('run');
   if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
     {
         if (this.body.onFloor())
         {
             this.body.velocity.y = -200*this._jump;
+            this.animations.play('jump', 10, false);
         }
     }
-  this.animationshandler();
+  
  
 }
 
-Inkling.prototype.animationshandler=function(){
-  if(this.body.onFloor()){
-     if(this.body.velocity.x===0)this.animations.play('idle');
-     else this.animations.play('run');
-  }
-}
+
+
 
 Inkling.prototype.Movement=function (dir){
   if(this.scale.x*dir<0) this.scale.x=-this.scale.x;
   this.body.velocity.x=dir*this._speed;
 }
+
+
 
 
 
@@ -82,7 +91,7 @@ var map; var layer;
     if(this.game.input.keyboard.isDown(Phaser.Keyboard.O)) map.replace(1,2);
     else if(this.game.input.keyboard.isDown(Phaser.Keyboard.W)) map.replace(2,1);
 
-    //console.log(Inklingsprite.body.velocity);
+    console.log(Inklingsprite.body.velocity.y);
    
   
     

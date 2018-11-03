7a700e5b2d5a357febfe4fdff812f525823face6
BraisMorgade <36987428+BraisMorgade@users.noreply.github.com>
2018-11-03 16:29:06 +0100

    FixedBug

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,5 +1,8 @@
 'use strict'
 
+
+
+
 var Inkling= function (game, x, y, sprite, speed, jump){
   //Atributos
   Phaser.Sprite.call(this, game, x, y, sprite);
@@ -8,10 +11,19 @@ var Inkling= function (game, x, y, sprite, speed, jump){
   this._health=100;
   this._ammo=100;
 
+  this.iskid=true;
+
+  
+  //Controles
+  this.mrightkey=Phaser.Keyboard.RIGHT;
+  this.mleftkey=Phaser.Keyboard.LEFT;
+  this.jumpkey=Phaser.Keyboard.UP;
+  this.transkey=Phaser.Keyboard.DOWN;
+  this.shootkey=Phaser.Keyboard.SPACE;
+
   //Físicas
   this.game.physics.arcade.enable(this);
   this.body.collideWorldBounds=true;
-  this.body.collideWorldBounds;
   this.body.gravity.y=800;
   this.body.velocity.x=0;
   this.body.velocity.y=0;
@@ -20,11 +32,15 @@ var Inkling= function (game, x, y, sprite, speed, jump){
   this.game.add.existing(this);
   this.frame=0;
   this.animations.add('idle',[0,1,2,3,4,5,6,7,8,9], 9, true);
-  this.animations.add('jump', [10,11,12,13,14,15], 9, true);
+  this.animations.add('jump', [10,11,12,13,14,15], 9, false);
   this.animations.add('run', [20,21,22,23,24,25,26,27], 9, true);
-  this.animations.add('transform', [30,31,32,33,34], 9, true);
-  this.animations.add('fall',[70,71],9,true);
-  this.animations.add('ground',[80,81,82],9,true);
+  this.animations.add('transform', [30,31,32,33,34], 9, false);
+  this.animations.add('detransform', [34,33,32,31,30], 9, true);
+  this.animations.add('movesquid', [60,61,62,63], 9, true);
+  this.animations.add('fall', [70,71], 9, true);
+  this.currentAnimation ='idle';
+  
+
   this.anchor.setTo(.5,.5);
   this.scale.setTo(this.scale.x * 2, this.scale.y *2);
   
@@ -34,68 +50,69 @@ Inkling.prototype.constructor=Inkling;
 
 //Métodos Inkling
 
-var suelo = true;
-var aux ;
 Inkling.prototype.update=function(){
   var dir=0;
-  if(this.body.velocity.y ===0 && this.body.velocity.y <14 && this.body.velocity.y>13) suelo = true;
-  else suelo = false;
-  if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) dir=-1;
-  else if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) dir=1;
+  //movimiento
+  if(this.game.input.keyboard.isDown(this.mrightkey)) dir=1;
+  else if(this.game.input.keyboard.isDown(this.mleftkey)) dir=-1;
   this.Movement(dir);
-  if(this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
-    this.animations.play('transform', 9, false);
-  } 
-  else if(this.body.onFloor() && this.body.velocity.x===0 && !aux) this.animations.play('idle');
-  else if(this.body.onFloor() && this.body.velocity.x !==0) this.animations.play('run');
-  if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
+  //Salto
+  if(this.body.onFloor()&&this.game.input.keyboard.isDown(this.jumpkey)) this.body.velocity.y=this._jump;
+  //transformación
+  if(this.game.input.keyboard.isDown(this.transkey)) this.iskid=false;
+  else this.iskid=true;
+  //animar
+  this.Animator();
+}
+
+Inkling.prototype.Animator=function()
+{
+  if(!this.iskid) this.animations.play('movesquid', 9, false);
+  else
+  {
+    if(this.body.onFloor())
     {
-        if (this.body.onFloor())
-        {
-            this.body.velocity.y = -200*this._jump;
-            this.animations.play('jump', 10, false);
-        }
+      if(this.body.velocity.x===0) this.animations.play('idle');
+      else this.animations.play('run');
     }
-  //if(suelo===false && this.body.velocity.y>0 ){this.animations.play('fall',5,false);aux =true;}
-  //if(suelo === true && aux ===true ) {this.animations.play('ground',9,false);aux = false}
-  console.log(this.body.velocity.y ===0 );
-  
+    else
+    {
+      if(this.body.velocity.y<=0) this.animations.play('jump', 9, false);
+      else this.animations.play('fall', 9, false);
+    }
   }
-
-
-
-
-Inkling.prototype.Movement=function (dir){
-  if(this.scale.x*dir<0) this.scale.x=-this.scale.x;
-  this.body.velocity.x=dir*this._speed;
 }
 
 
+Inkling.prototype.Movement=function (dir, dt)
+{
+  if(this.scale.x*dir<0) this.scale.x=-this.scale.x;
+  this.body.velocity.x=dir*this._speed;
 
+}
 
 
 var Inklingsprite;
 var map; var layer;
   var PlayScene = {
   create: function () {
-   Inklingsprite= new Inkling (this.game, this.game.world.centerX, 0, 'Inkling',400,3);
+   Inklingsprite= new Inkling (this.game, this.game.world.centerX, 0, 'Inkling',400,-520);
    map= this.game.add.tilemap('tilemap');
    map.addTilesetImage('tileset');
    layer= map.createLayer('Capa de Patrones 1');
    map.setCollision([1,2]);
    layer.resizeWorld();
    this.game.camera.follow(Inklingsprite);
-
    this.physics.startSystem(Phaser.Physics.ARCADE);
-  
+   
   },
   update: function () {
-    Inklingsprite.update();
     this.game.physics.arcade.collide(Inklingsprite, layer);
+    Inklingsprite.update();
     if(this.game.input.keyboard.isDown(Phaser.Keyboard.O)) map.replace(1,2);
     else if(this.game.input.keyboard.isDown(Phaser.Keyboard.W)) map.replace(2,1);
+    console.log(Inklingsprite.body.velocity.y);
 
-  //console.log(Inklingsprite.body.velocity.y);
    
   
     

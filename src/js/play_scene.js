5365174981e2b15d55e0629f2ab0e740ef4d0418
MiguelZh <37276857+MiguelZh@users.noreply.github.com>
2018-11-18 19:08:54 +0100

    barra de vida completa
    falta implementar el daño automatico , funcion hecha de todas maneras

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,103 +1,67 @@
 'use strict'
 
-//clase pool 
-
-function ShotsPool(game, entities) {
+function Pool(game, entities) {
   this._group = game.add.group();
   this._group.addMultiple(entities);
   this._group.callAll('kill');
 }
 
-ShotsPool.prototype.spawn = function (x, y, sprite, dir) {
-  var entity = this._group.getFirstExists(false);
-  if (entity) {
-    entity.revive();
-
-    entity.initialize(x, y, sprite, dir);
-  }
-  return entity;
+Pool.prototype.spawn=function(x,y){
+    var entity = this._group.getFirstExists(false);
+    if (entity) {
+      entity.reset(x, y);
+    }
+    return entity;
 }
 
-ShotsPool.prototype.forEachAlive = function (funcion) {
+Pool.prototype.forEachAlive=function(funcion){
   this._group.forEachAlive(funcion);
 }
-
-
-
-
-var Inkling = require('./Inkling.js');
-var player;
-var shots;
-var shot = require('./Shot.js');
-
-var PlayScene = {
-  ////////CREATE//////////
-  create: function () {
-    //creacion de array de balas
-    var bullets = [];
-    for (var i = 0; i < 100; i++) {
-      bullets.push(new shot(this.game));
-      bullets[i].kill();
-    }
-
-    //creacion del pool de balas 
-    shots = new ShotsPool(this.game, bullets);
-
-    //creacion del mapa
-    this.map = this.game.add.tilemap('tilemap');
-    this.map.addTilesetImage('tileset');
-    this.layer = this.map.createLayer('Capa de Patrones 1');
-    this.map.setCollision([1, 2]);
-    this.layer.resizeWorld();
-
-    //creacion del jugador
-    player = new Inkling(this.game, this.game.world.centerX, 0, 'Inkling', 400, -600);
-
-
-    //seguimiento de cámara(temporal)
-    this.game.camera.follow(player);
-
-    //activacion del sistema de físicas
-    this.physics.startSystem(Phaser.Physics.ARCADE);
-
-  },
-
-  ////////UPDATE/////////
-  update: function () {
-    //colisiones jugador, mapa
-    this.game.physics.arcade.collide(player, this.layer);
-
-    //actualizacion de estado del jugador
-    player.update(shots);
-    this.game.debug.body(player);
-
-    var TileGround= this.map.getTile(Math.floor(player.x/64), Math.floor((player.y+(player.height/2)+15)/64));
-    if(TileGround!==null){
-      if(TileGround.index===2){
-        player.squidspeed=player.kidspeed*1.75;
-      }
-      else player.squidspeed=player.kidspeed*0.5;
-    }
   
 
 
-    //colisiones balas con mapa
-    self = this;
-    shots.forEachAlive(function (each) {
-      self.game.physics.arcade.collide(each, self.layer, function () {
-        var dir = 0;
-        var rot = 0;
-        if (each.scale.x > 0) dir = 1;
-        else if (each.scale.x < 0) dir = -1;
-        if (each.rotation > 0) rot = 1;
-        else if (each.rotation < 0) rot = -1;
-        //pintar tile(cambia el index del tile por uno de color)
+var HealthBar = require('./HealthBar.js');
+var Inkling = require('./Inkling.js');
+var player;
+var map; var layer;
+var pool;
+var shot=require('./Shot.js');
 
-        self.map.replace(1, 2, Math.floor((each.x + dir * 15 + ((each.width / 2))) / 64), Math.floor((each.y + dir * rot * (each.height / 2) + 15 * rot * dir) / 64), 1, 1, self.layer);
-        each.kill();
-      })
-    });
+  var PlayScene = {
+  create: function () {
+    //barra de vida en pantalla
+    var barConfig = {x: 200, y: 100};
+    this.myHealthBar = new HealthBar(this.game, barConfig);
+    this.myHealthBar.setPercent(50); // baja la barra de vida
 
+
+
+    var bullets= [];
+    for (var i=0; i<100; i++){
+      bullets.push(new shot(this.game));
+      
+    }
+    console.log(bullets);
+    pool=new Pool(this.game, bullets);
+    map= this.game.add.tilemap('tilemap');
+    map.addTilesetImage('tileset');
+    layer= map.createLayer('Capa de Patrones 1');
+    map.setCollision([1,2]);
+    layer.resizeWorld();
+    player= new Inkling (this.game, this.game.world.centerX, 0, 'Inkling',400,-600, 'bullet');
+  
+   this.game.camera.follow(player);
+   this.physics.startSystem(Phaser.Physics.ARCADE);
+   
+  },
+  update: function () {
+
+    this.game.physics.arcade.collide(player,layer);
+    player.update(pool);
+    self=this;
+    pool.forEachAlive(function(item){ item.update(); self.game.physics.arcade.collide(item, self.game.layer, item.kill()); });
+    if(this.game.input.keyboard.isDown(Phaser.Keyboard.O)) map.replace(1,2);
+    else if(this.game.input.keyboard.isDown(Phaser.Keyboard.W)) map.replace(2,1);
   }
 
 };

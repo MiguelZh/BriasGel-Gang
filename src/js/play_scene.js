7b23e4b1280613b8baee3b4ab75647707085d797
MiguelZh <37276857+MiguelZh@users.noreply.github.com>
2018-12-09 17:30:59 +0100

    gamepad implementado
    falta optimizar todo funciona

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -36,11 +36,7 @@ var PlayScene = {
       bullets[i].kill();
     }
     // inicializacion del gamepad
-    this.game.input.gamepad.start();
-    this.pad1=this.game.input.gamepad.pad1;
-    if(this.game.input.gamepad.supported && this.game.input.gamepad.active ){
-      console.log("Hola!!!!!");
-    }
+ 
 
     //creacion del pool de balas 
     this.shots = new ShotsPool(this.game, bullets);
@@ -52,8 +48,8 @@ var PlayScene = {
     this.layer.resizeWorld();
     this.map.setCollision([1, 2]);
     //creacion de jugadores
-    this.player1 = new Inkling(this.game, this.game.world.centerX + 150, 0, 'Inklingo', 300, -400, Phaser.Keyboard.RIGHT, Phaser.Keyboard.LEFT, Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.CONTROL, Phaser.Keyboard.R, Phaser.Keyboard.T, 2);
-    this.player2 = new Inkling(this.game, this.game.world.centerX - 150, this.game.world.centerY, 'Inklingp', 300, -400, Phaser.Gamepad.XBOX360_DPAD_LEFT || Phaser.Gamepad.XBOX360_STICK_LEFT_X, Phaser.Gamepad.XBOX360_STICK_LEFT_X || Phaser.Gamepad.XBOX360_STICK_LEFT_X, Phaser.Gamepad.XBOX360_DPAD_UP || Phaser.Gamepad.XBOX360_STICK_LEFT_Y, Phaser.Gamepad.XBOX360_DPAD_DOWN || Phaser.Gamepad.XBOX360_STICK_LEFT_Y, Phaser.Keyboard.SPACEBAR, Phaser.Keyboard.G, Phaser.Keyboard.F, 3);
+    this.player1 = new Inkling(this.game, this.game.world.centerX + 150, 0, 'Inklingo', 300, -400, Phaser.Keyboard.RIGHT, Phaser.Keyboard.LEFT, Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.CONTROL, Phaser.Keyboard.R, Phaser.Keyboard.T, 2,false);
+    this.player2 = new Inkling(this.game, this.game.world.centerX - 150, this.game.world.centerY, 'Inklingp', 300, -400, Phaser.Gamepad.XBOX360_DPAD_LEFT , Phaser.Gamepad.XBOX360_STICK_LEFT_X, Phaser.Gamepad.XBOX360_DPAD_UP , Phaser.Gamepad.XBOX360_DPAD_DOWN , Phaser.Keyboard.SPACEBAR, Phaser.Keyboard.G, Phaser.Keyboard.F, 3,true);
 
     //creacion de interfaz
     var backgroundhud = new Interface(this.game, this.game.world.centerX, 50, 'hud', 60, 230);
@@ -95,6 +91,57 @@ var PlayScene = {
     this.middlepoint.x=(this.player1.x+this.player2.x)/2;
     this.middlepoint.y=(this.player1.y+this.player2.y)/2;
 
+  /*  //controles mando
+    if (this.pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_LEFT) || this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) < -0.1)
+    {
+        console.log("Se detecto, left");
+    }
+    else if (this.pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_RIGHT) || this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_X) > 0.1)
+    {
+       console.log("Se detecto, right");
+    }
+
+    if (this.pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_UP) || this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.1)
+    {
+        console.log("Se detecto, up");
+
+    }
+    else if (this.pad1.isDown(Phaser.Gamepad.XBOX360_DPAD_DOWN) || this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.1)
+    {
+      console.log("Se detecto, down");
+
+    }
+
+    if (this.pad1.justPressed(Phaser.Gamepad.XBOX360_A))
+    {
+      console.log("Se detecto, jump");
+
+    }
+
+    if (this.pad1.justReleased(Phaser.Gamepad.XBOX360_B))
+    {
+      console.log("Se detecto, tranform");
+
+    }
+
+    if (this.pad1.connected)
+    {
+        var rightStickX = this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_X);
+        var rightStickY = this.pad1.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y);
+
+        if (rightStickX)
+        {
+          console.log("Se detecto, vertical");
+
+        }
+
+        if (rightStickY)
+        {
+          console.log("Se detecto, vertical");
+
+        }
+    }*/
+    //fin mando
     this.players.forEach(function (player) {
       //colisiones jugadores, mapa
       self.game.physics.arcade.collide(player, self.layer);
